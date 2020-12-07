05188f4bb53825b68d7e7cbbde33aacb70ff968a
kshitij7890 <kshitijsingla7890@gmail.com>
2020-12-08 02:40:18 +0530

    Using Stateful widget correctly

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class MyAppState extends State<MyApp> {
   var questionIndex = 0;
 
   void answerQuestion() {
-    questionIndex = questionIndex + 1;
+    setState(() {
+      //to re-run build() when property is changed
+      questionIndex = questionIndex + 1;
+    });
+
     print(questionIndex);
   }
 
