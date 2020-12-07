da0cb00d93217f68c4afaaee8d3dbabbd76e80f4
kshitij7890 <kshitijsingla7890@gmail.com>
2020-12-08 02:19:48 +0530

    Using Stateless widget incorrectly

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+  }
+
   @override // decorator to make code cleaner
   Widget build(BuildContext context) {
     var questions = [
@@ -18,18 +25,21 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The Ques'),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: null,
+            Text(
+              questions[questionIndex],
             ),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed:
+                  answerQuestion, //dont use parenthesis here(a pointer to the function will be passed)
             ),
             RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: null,
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
             ),
           ],
         ), // commas at end -> proper formation of code tree
